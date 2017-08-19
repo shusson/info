292312184b3856d0b43be14a89eb780e05ccf0af
Shane Husson <shane.a.husson@gmail.com>
2017-08-19 22:47:10 +1000

    add dates
    add pics

--- src/app/p18082017/p18082017.component.ts
+++ src/app/p18082017/p18082017.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-p18082017',
-  templateUrl: './p18082017.component.html',
-  styleUrls: ['./p18082017.component.css']
+    selector: 'app-p18082017',
+    templateUrl: './p18082017.component.html',
+    styleUrls: ['./p18082017.component.css']
 })
 export class P18082017Component implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
