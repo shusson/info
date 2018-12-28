ddbc2106a849e44bfe80570526cb77d1186f5334
shusson <shane.a.husson@gmail.com>
2018-12-28 18:46:07 +0100

    dep update

--- src/app/blog.service.ts
+++ src/app/blog.service.ts
@@ -1,22 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/map';
-import { environment } from '../environments/environment';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../environments/environment";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 export class Post {
-    constructor(public title = "",
-                public body = "",
-                public slug = "",
-                public timestamp = 0,
-                public updated = 0,
-                public uuid = "",
-                public image = "") {}
+    constructor(
+        public title = "",
+        public body = "",
+        public slug = "",
+        public timestamp = 0,
+        public updated = 0,
+        public uuid = "",
+        public image = ""
+    ) {}
 
     date() {
         const date = new Date(this.timestamp);
-        return [date.getDate(), date.getMonth() + 1 , date.getFullYear()];
+        return [date.getDate(), date.getMonth() + 1, date.getFullYear()];
     }
 
     url() {
@@ -30,26 +31,42 @@ export class Post {
 
 @Injectable()
 export class BlogService {
-
-    constructor(private http: HttpClient) {
-
-    }
+    constructor(private http: HttpClient) {}
 
     getPosts(): Observable<Post[]> {
-        return this.http.get(`${environment.blogCMS}/contents?type=Post`)
-            .map((v: any) => {
+        return this.http.get(`${environment.blogCMS}/contents?type=Post`).pipe(
+            map((v: any) => {
                 return v.data.map(p => {
-                    return new Post(p.title, p.body, p.slug, p.timestamp, p.updated, p.uuid, p.image);
+                    return new Post(
+                        p.title,
+                        p.body,
+                        p.slug,
+                        p.timestamp,
+                        p.updated,
+                        p.uuid,
+                        p.image
+                    );
                 });
-            });
+            })
+        );
     }
 
     getPost(slug: string): Observable<Post> {
-        return this.http.get(`${environment.blogCMS}/content?slug=${slug}`)
-            .map((v: any) => {
-                const p = v.data[0];
-                return new Post(p.title, p.body, p.slug, p.timestamp, p.updated, p.uuid, p.image);
-            });
+        return this.http
+            .get(`${environment.blogCMS}/content?slug=${slug}`)
+            .pipe(
+                map((v: any) => {
+                    const p = v.data[0];
+                    return new Post(
+                        p.title,
+                        p.body,
+                        p.slug,
+                        p.timestamp,
+                        p.updated,
+                        p.uuid,
+                        p.image
+                    );
+                })
+            );
     }
-
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,28 +1,26 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { BlogService, Post } from '../blog.service';
-import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-
-import 'rxjs/add/operator/switchMap';
+import { Component, OnInit, Input } from "@angular/core";
+import { BlogService, Post } from "../blog.service";
+import { ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
 
 @Component({
-    selector: 'app-post',
-    templateUrl: './post.component.html',
-    styleUrls: ['./post.component.css'],
+    selector: "app-post",
+    templateUrl: "./post.component.html",
+    styleUrls: ["./post.component.css"],
     providers: [BlogService]
 })
 export class PostComponent implements OnInit {
     post: Post;
 
-    constructor(private bs: BlogService,
-                private route: ActivatedRoute) {
-    }
+    constructor(private bs: BlogService, private route: ActivatedRoute) {}
 
     ngOnInit() {
         this.route.paramMap
-            .switchMap((params: ParamMap) => {
-                return this.bs.getPost(params.get('slug'));
-            })
-            .subscribe(post => this.post = post);
+            .pipe(
+                switchMap((params: ParamMap) => {
+                    return this.bs.getPost(params.get("slug"));
+                })
+            )
+            .subscribe(post => (this.post = post));
     }
-
 }
